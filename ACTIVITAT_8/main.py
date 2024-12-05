1e50f6c7eedabf1bdb6bcf9d969832714e73c658
rdalmau23 <rdalmau23@ilg.cat>
2024-12-05 09:38:26 +0100

    httpexception

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
 items = {}
-
 
 class Item(BaseModel):
     name: str
@@ -14,8 +13,11 @@ class Item(BaseModel):
     stock: int
     is_available: bool = True  
 
+
 @app.get("/items/{item_id}")
 def get_item(item_id: int):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
     return items[item_id]
 
 @app.post("/items/")

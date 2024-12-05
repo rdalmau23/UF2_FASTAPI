2dafa8a7fb68c55d8990f3623c7cc697e78750c4
rdalmau23 <rdalmau23@ilg.cat>
2024-12-05 09:37:10 +0100

    post

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+items = {}
 
 
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float
+    stock: int
+    is_available: bool = True  
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    return items[item_id]
+
+@app.post("/items/")
+def create_item(item: Item):
+    item_id = len(items) + 1
+    items[item_id] = item.dict()
+    return {"id": item_id, "item": item}
